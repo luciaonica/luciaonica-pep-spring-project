0893e99a8837862b565d7bcc0d8ccb70bba91721
Lucia Onica <lucia12onica@gmail.com>
2024-09-19 12:31:02 +0000

    Completed getAllMessages

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,6 +66,12 @@ public class SocialMediaController {
 		} catch(Exception e) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}	
+	}
+
+	@GetMapping("/messages")
+	public List<Message> getAllMessages() {
+		
+		return messageService.getAllMessages();		
 	}
 
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,11 @@ public class MessageService {
               
             throw new AccountNotFoundException("invalid message format");
         }
+    }
+
+    public List<Message> getAllMessages() {
+			
+        return messageRepository.findAll();
     }
 
 }
